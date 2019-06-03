9f9d1da63f863e1467d54f5457efbb60c1da9693
Zewen Wang <w.zewen@outlook.com>
2019-06-03 18:25:35 -0400

    http request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,33 @@ import { AppComponent } from "./app.component";
 import { ProductListComponent } from "./products/product-list.component";
 import { ConvertToSpacesPipe } from "./shared/convert-to-spaces.pipe";
 import { StarComponent } from "./shared/star.component";
+import { HttpClientModule } from "@angular/common/http";
+import { ProductDetailComponent } from "./products/product-detail.component";
+import { WelcomeComponent } from "./home/welcome.component";
 
+import { RouterModule } from "@angular/router";
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: "products", component: ProductListComponent },
+      { path: "products/:id", component: ProductDetailComponent },
+      { path: "welcome", component: WelcomeComponent },
+      { path: "", redirectTo: "welcome", pathMatch: "full" },
+      { path: "**", redirectTo: "welcome", pathMatch: "full" }
+    ])
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -11,6 +11,7 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
+  errMessage: string ='';
   _listFilter: string;
 
   get listFilter(): string {
@@ -24,30 +25,7 @@ export class ProductListComponent implements OnInit {
   }
   filteredProducts: IProduct[];
 
-  products: IProduct[] = [
-    {
-      productId: 1,
-      productName: "Leaf Rake",
-      productCode: "GDN-0011",
-      releaseDate: "March 19, 2016",
-      description: "Leaf rake with 48-inch wooden handle.",
-      price: 19.95,
-      starRating: 3.2,
-      imageUrl:
-        "http://bot-framework.azureedge.net/bot-icons-v1/Chatbots_AskMinDy_WebApp_D3n8m6FFmCMMCICCTC8zpCHFFbkDTUGFU71xAWOch5tKB5v.png"
-    },
-    {
-      productId: 2,
-      productName: "Garden Cart",
-      productCode: "GDN-0023",
-      releaseDate: "March 18, 2016",
-      description: "15 gallon capacity rolling garden cart",
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl:
-        "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-    }
-  ];
+  products: IProduct[] = [];
   constructor(private productService: ProductService) {}
   performFilter(filterBy: string): IProduct[] {
     filterBy = filterBy.toLocaleLowerCase();
@@ -60,12 +38,18 @@ export class ProductListComponent implements OnInit {
     this.showImage = !this.showImage;
   }
   ngOnInit(): void {
-    this.products = this.productService.getProducts();
-    this.filteredProducts = this.products;
-    this.listFilter = "";
-    console.log("Hello from ngOnInit");
+    this.productService.getProducts().subscribe(
+      products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error => this.errMessage = <any>error
+    );
+    this.listFilter = '';
   }
+
+  
   onRatingClicked(message: string): void {
-    this.pageTitle = "Product List " + message;
+    this.pageTitle = 'Product List ' + message;
   }
 }
